a3ed4f80f3047f7c818a8ab00332f5632497920c
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-04 11:09:30 -0800

    added drill 3 and 4 code

--- DSArray.js
+++ DSArray.js
@@ -96,11 +96,17 @@ function main(){
 
   // Add an item to the array
   arr.push(3);
-  arr.push(5);
-  arr.push(15);
-  arr.push(19);
-  arr.push(45);
-  arr.push(10);
+  // arr.push(5);
+  // arr.push(15);
+  // arr.push(19);
+  // arr.push(45);
+  // arr.push(10);
+
+  // arr.pop();
+  // arr.pop();
+  // arr.pop();
+
+  // arr.push('tauhida');
 
   console.log(arr);
 }
